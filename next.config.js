df18186cfe4c21744139db7d3b4ef08f7235193d
honai <29170100+honai@users.noreply.github.com>
2019-06-08 01:10:50 +0900

    並列型チェック導入

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const withTypescript = require('@zeit/next-typescript')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 
-module.exports = withTypescript()
+module.exports = withTypescript({
+  webpack(config, options) {
+    if (options.isServer) config.plugins.push(new ForkTsCheckerWebpackPlugin())
+    return config
+  }
+})
