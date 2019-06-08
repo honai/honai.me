1fe8685de24ca6f3ad5a43402edb8c7972b965b7
honai <29170100+honai@users.noreply.github.com>
2019-06-09 02:08:00 +0900

    redux devtools support

--- Redux/index.ts
+++ Redux/index.ts
@@ -1,6 +1,15 @@
 import { createStore as reduxCreateStore, combineReducers, Store } from 'redux'
 import counter from './modules/counter'
 
+const rootReducer = combineReducers({ counter })
+
 export default function createStore(): Store {
-  return reduxCreateStore(combineReducers({ counter }))
+  if (typeof window !== 'undefined' && process.env.NODE_ENV !== 'production') {
+    return reduxCreateStore(
+      rootReducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  } else {
+    return reduxCreateStore(rootReducer)
+  }
 }
