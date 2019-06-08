6dba067136bf40d9e68d6edf31b1f7ef444b2d32
honai <29170100+honai@users.noreply.github.com>
2019-06-09 06:40:25 +0900

    contentful周りの型宣言

--- api/contentful.ts
+++ api/contentful.ts
@@ -1,34 +1,16 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const contentfulSdk = require('contentful')
-const contentful = contentfulSdk.createClient({
+import { createClient, Entry } from 'contentful'
+
+const contentful = createClient({
   space: process.env.CTF_SPACE_ID,
   accessToken: process.env.CTF_ACCESSTOKEN
 })
 
 export default contentful
 
-export function getBlogPosts(limit: number, skip: number) {
-  return contentful.getEntries({
-    // eslint-disable-next-line @typescript-eslint/camelcase
-    content_type: 'blogPosts',
-    limit: limit,
-    skip: skip
-  })
-}
+export type Post = Entry<PostFields>
 
-export interface GetEntries {
-  items: Post[]
-}
-
-export interface Post {
-  sys: {
-    id: string
-    createdAt: string
-    updatedAt: string
-  }
-  fields: {
-    slug: string
-    title: string
-    content: string
-  }
+export interface PostFields {
+  slug: string
+  title: string
+  content: string
 }
