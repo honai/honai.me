6e082d636816d4f31adad38679df8106ed698ffc
honai <29170100+honai@users.noreply.github.com>
2019-06-10 09:32:52 +0900

    add page component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,20 +2,19 @@ import { NextFC } from 'next'
 import { useSelector, useDispatch } from 'react-redux'
 import Link from 'next/link'
 import { RootState } from 'Redux'
+import Page from '../components/page'
 
 const Index: NextFC = (): JSX.Element => {
-  const count = useSelector((state: RootState) => state.counter.count)
-  const dispatch = useDispatch()
   return (
-    <div>
+    <Page>
       <h1>Hello TS</h1>
-      <p>Count: {count}</p>
-      <button onClick={() => dispatch({ type: 'INCREMENT' })}>ADD</button>
+      <p>Count:</p>
+      <button>ADD</button>
       <br />
       <Link href="/blog">
         <a>BLOG</a>
       </Link>
-    </div>
+    </Page>
   )
 }
 
