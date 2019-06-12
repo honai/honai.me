37c33ee8586a58eea510bffc1d85b893e12b47ed
honai <29170100+honai@users.noreply.github.com>
2019-06-12 20:47:56 +0900

    add Hero and profile picture

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,18 @@ import { NextFC } from 'next'
 import Page from '../components/Page'
 import Header from '../components/Header'
 import Main from '../components/Main'
+import Hero from '../components/Hero'
+import Section from '../components/Section'
 
 const Index: NextFC = (): JSX.Element => {
   return (
     <Page>
+      <Hero />
       <Header />
       <Main>
-        <h1>Hello TypeScript!</h1>
+        <Section title="自己紹介">
+          <p>京都大学工学部電気電子工学科3回生。</p>
+        </Section>
       </Main>
     </Page>
   )
