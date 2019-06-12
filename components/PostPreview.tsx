b1e4a193b74687e5e146764ba508ab13efaef603
honai <29170100+honai@users.noreply.github.com>
2019-06-13 04:19:28 +0900

    add post list component

---
+++ components/PostPreview.tsx
@@ -0,0 +1,60 @@
+import Link, { LinkProps } from 'next/link'
+import moment from 'moment'
+import { primaryColor } from './theme'
+import { Post } from '../api/contentful'
+
+moment.locale('ja')
+
+interface PostLinkProps {
+  slug: string
+}
+
+const PostLink = ({ children, slug }: LinkProps & PostLinkProps): JSX.Element => (
+  <Link href={`/blog/post?slug=${slug}`} as={`/blog/post/${slug}`}>
+    {children}
+  </Link>
+)
+
+interface PreviewProps {
+  post: Post
+}
+
+const PostPreview = ({ post }: PreviewProps): JSX.Element => {
+  const { slug, title } = post.fields
+  const { createdAt } = post.sys
+  return (
+    <section>
+      <PostLink slug={slug}>
+        <a>
+          <h2>{title}</h2>
+          <div className="date">{moment(createdAt).format('ll')}</div>
+        </a>
+      </PostLink>
+      <style jsx>{`
+        a {
+          text-decoration: unset;
+        }
+        a:hover h2,
+        a:hover .date {
+          color: ${primaryColor};
+        }
+        section {
+          box-shadow: 0 2px 3px rgba(0, 0, 0, 0.3);
+          padding: 10px 20px;
+          border-radius: 5px;
+          background-color: #fff;
+        }
+        h2,
+        .date {
+          text-align: center;
+        }
+        .date {
+          color: #888;
+          margin: 1rem;
+        }
+      `}</style>
+    </section>
+  )
+}
+
+export default PostPreview
