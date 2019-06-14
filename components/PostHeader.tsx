120eecf15868cf9c247dbe4f6d96d96378a28b79
honai <29170100+honai@users.noreply.github.com>
2019-06-15 06:00:22 +0900

    add publish & update date

--- components/PostHeader.tsx
+++ components/PostHeader.tsx
@@ -1,25 +1,56 @@
-import moment from 'moment'
+import { Time } from '../atoms/Time'
 
-moment.locale('ja')
-
-interface Props {
+interface PostHeaderProps {
   title: string
   published: Date
   updated: Date
 }
-const PostHeader = (props: Props): JSX.Element => (
+
+const Publish = ({ published }: { published: Date }): JSX.Element => (
+  <span style={{ padding: '0 5px' }}>
+    <Time time={published} format="l" />
+  </span>
+)
+
+const Update = ({ updated }: { updated: Date }): JSX.Element => (
+  <span>
+    <i className="material-icons">update</i>
+    <Time time={updated} format="l" />
+    <style jsx>{`
+      i {
+        font-size: 1em;
+        padding: 0 3px;
+      }
+      span {
+        display: flex;
+        align-items: center;
+      }
+    `}</style>
+  </span>
+)
+
+const PostHeader = (props: PostHeaderProps): JSX.Element => (
   <div>
     <h2>{props.title}</h2>
-    <p>
-      <span>{moment(props.published).format('ll')}</span>
-    </p>
+    <div className="dates century-gothic">
+      <Publish published={props.published} />
+      ・
+      <Update updated={props.updated} />
+    </div>
     <style jsx>{`
       h2 {
-        text-align: center;
-        margin: 0;
-        padding: 30px;
-        color: #eee;
         background-color: #333;
+        font-weight: normal;
+        text-align: center;
+        color: #eee;
+        margin: 0;
+        padding: 30px 10px;
+      }
+      .dates {
+        color: #777;
+        display: flex;
+        justify-content: end;
+        padding: 5px 10px 0;
       }
     `}</style>
   </div>
--- pages/blog/post.tsx
+++ pages/blog/post.tsx
@@ -28,8 +28,9 @@ const PostPage: NextPage<InitialProps, Query> = (
     post = posts.find((post): boolean => post.fields.slug === slug)
   }
   return (
-    <Page>
+    <Page title={post ? post.fields.title : 'NOT FOUND'}>
       <Head>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.10.1/dist/katex.css" />
       </Head>
       <Header />
