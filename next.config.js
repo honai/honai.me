ea995383fa56992edea38682088235def12b351e
honai <29170100+honai@users.noreply.github.com>
2019-06-15 04:11:49 +0900

    remove api key from code

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 module.exports = withTypescript({
   env: {
     CTF_SPACE_ID: '7q1ibtbymdj9',
-    CTF_ACCESSTOKEN: '751eaa5a3ed1043c5b017aa036a0819b25148a73bb1c9121d70058c98e53c2f7'
+    CTF_ACCESSTOKEN: process.env.CTF_ACCESSTOKEN
   },
   webpack(config, options) {
     if (options.isServer) config.plugins.push(new ForkTsCheckerWebpackPlugin())
