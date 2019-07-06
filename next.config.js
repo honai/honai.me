41a596ac481fe7aae6524d7ca5d0d266fb5ae7b8
honai <29170100+honai@users.noreply.github.com>
2019-07-07 04:15:22 +0900

    configure for buildtime env vals

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const withTypescript = require('@zeit/next-typescript')
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 
 module.exports = withTypescript({
+  env: {
+    CTF_SPACE_ID: process.env.CTF_SPACE_ID,
+    CTF_ACCESSTOKEN: process.env.CTF_ACCESSTOKEN
+  },
   webpack(config, options) {
     if (options.isServer) config.plugins.push(new ForkTsCheckerWebpackPlugin())
     return config
