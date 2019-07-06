69d43c1f663e6f1d68b33c74d7a453de1742a44c
honai <29170100+honai@users.noreply.github.com>
2019-07-07 04:07:59 +0900

    configure environment variables

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ const withTypescript = require('@zeit/next-typescript')
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 
 module.exports = withTypescript({
-  env: {
-    CTF_SPACE_ID: '7q1ibtbymdj9',
-    CTF_ACCESSTOKEN: process.env.CTF_ACCESSTOKEN
-  },
   webpack(config, options) {
     if (options.isServer) config.plugins.push(new ForkTsCheckerWebpackPlugin())
     return config
