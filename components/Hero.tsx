62f7dfa253228096d7d288224ee45c72dbbfeef7
honai <29170100+honai@users.noreply.github.com>
2019-07-12 02:39:25 +0900

    threshold hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,64 +2,50 @@ const Hero = (): JSX.Element => {
   return (
     <div className="hero">
       <div className="icon">
-        <img src="/static/profile.png" alt="アイコン" height="100%" />
+        <img src="/static/profile.png" alt="アイコン" width="100%" />
       </div>
-      <h1 className="century-gothic">Honai Ueoka</h1>
-      <h2 className="century-gothic">Student at Kyoto University.</h2>
-      <div className="scroll-icon" />
+      <div className="titles">
+        <h1 className="century-gothic">Honai Ueoka</h1>
+        <div className="subtitle century-gothic">Student at Kyoto University.</div>
+      </div>
       <style jsx>{`
         .hero {
           align-items: center;
           background-color: #02545a;
           color: #fff;
           display: flex;
-          flex-flow: column nowrap;
-          padding: 20px 0;
+          flex-flow: row nowrap;
+          justify-content: center;
+          padding: 10px;
           text-align: center;
         }
         .icon {
-          max-height: 180px;
-          height: 30vw;
+          flex: 0 1 80px;
         }
         .icon img {
           border-radius: 50%;
           max-height: 100%;
           max-width: 100%;
         }
+        .titles {
+          padding-left: 20px;
+          white-space: nowrap;
+        }
         h1 {
           border-bottom: 1px solid rgba(255, 255, 255, 0.5);
-          font-size: 2.5rem;
-          margin: 1rem 0 0.5rem;
-          width: 90%;
-          max-width: 400px;
-        }
-        h2 {
-          font-size: 1.2rem;
+          font-size: 2rem;
           margin: 0 0 0.5rem;
         }
-        .scroll-icon {
-          height: 24px;
-          width: 24px;
-          position: relative;
+        .subtitle {
+          margin: 0 0 0.5rem;
         }
-        .scroll-icon::before,
-        .scroll-icon::after {
-          content: '';
-          display: block;
-          width: 10px;
-          height: 10px;
-          border: 1px solid #fff;
-          border-bottom-style: none;
-          border-right-style: none;
-          transform: rotate(45deg);
-          position: absolute;
-          left: 7px;
-        }
-        .scroll-icon::before {
-          top: 8px;
-        }
-        .scroll-icon::after {
-          top: 13px;
+        @media screen and (min-width: 576px) {
+          h1 {
+            font-size: 2.5rem;
+          }
+          .icon {
+            flex: 0 1 90px;
+          }
         }
       `}</style>
     </div>
