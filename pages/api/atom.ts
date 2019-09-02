5192c5a74541a12f35ca735f1b9a48bd2f714fa5
honai <ueoka@honai.me>
2019-09-02 09:36:44 +0900

    use api routes feature

---
+++ pages/api/atom.ts
@@ -0,0 +1,11 @@
+import { ServerResponse, ClientRequest } from 'http'
+import generateFeed from '../../api/feeder'
+import { getAllBlogPosts } from '../../api/contentful'
+
+export default (_req: ClientRequest, res: ServerResponse): void => {
+  getAllBlogPosts().then((ctfRes): void => {
+    const feed = generateFeed(ctfRes.items)
+    res.setHeader('Content-Type', 'application/atom+xml; charset=utf-8')
+    res.end(feed.atom1())
+  })
+}
