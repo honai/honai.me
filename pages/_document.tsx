02602a057cc786c2436e962e74f74a1060227625
honai <29170100+honai@users.noreply.github.com>
2019-09-03 01:44:18 +0900

    Revert "enable gtag only in production"
    This reverts commit 82892fdfc9ae49940fb3bd47a9bfad880f87982c.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,19 +2,21 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
   public render(): JSX.Element {
-    const trackingId = location.hostname === 'honai.me' ? process.env.GA_TRACKING_ID : ''
     return (
       <Html lang="ja">
         <Head>
           <link rel="icon" type="image/x-icon" href="/static/favicon.ico" />
-          <script async src={`https://www.googletagmanager.com/gtag/js?id=${trackingId}`} />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}
+          />
           <script
             dangerouslySetInnerHTML={{
               __html: `
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
-                    gtag('config', '${trackingId}');
+                    gtag('config', '${process.env.GA_TRACKING_ID}');
                   `
             }}
           />
