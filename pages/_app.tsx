8a7fd1207f7735efc568f6450095d6972f0d60a9
honai <29170100+honai@users.noreply.github.com>
2019-09-02 12:35:08 +0900

    declare window.gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,12 +6,23 @@ import { Store } from 'redux'
 import withRedux from 'next-redux-wrapper'
 import createStore from 'src/store'
 
+declare global {
+  interface Window {
+    gtag(
+      arg0: string,
+      trackingId: string | undefined,
+      arg2: {
+        page_location: string
+      }
+    ): void
+  }
+}
+
 interface MyAppProps {
   store: Store
 }
 
-Router.events.on('routeChangeComplete', (url): void => {
-  // @ts-ignore
+Router.events.on('routeChangeComplete', (url: string): void => {
   window.gtag('config', process.env.GA_TRACKING_ID, {
     // eslint-disable-next-line @typescript-eslint/camelcase
     page_location: url
