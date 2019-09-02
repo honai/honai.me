1199dc31d495185aef8d0c41f352b511dfef5f31
honai <ueoka@honai.me>
2019-09-02 09:32:29 +0900

    remove _app Container

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import App, { Container } from 'next/app'
+import App from 'next/app'
 import Router from 'next/router'
 import { Provider } from 'react-redux'
 import { Store } from 'redux'
@@ -22,11 +22,9 @@ class MyApp extends App<MyAppProps> {
   public render(): JSX.Element {
     const { Component, pageProps, store } = this.props
     return (
-      <Container>
-        <Provider store={store}>
-          <Component {...pageProps} />
-        </Provider>
-      </Container>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
     )
   }
 }
