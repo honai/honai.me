37dff57db241dcf3096bf568ce0d592f1a8a2d54
honai <29170100+honai@users.noreply.github.com>
2019-09-04 13:42:07 +0900

    add OgpImage type def

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -16,6 +16,7 @@ export interface PostFields {
   content: string
   customPublishedAt: string
   customUpdatedAt: string
+  ogpImage: Asset
 }
 
 export function getBlogPosts(limit: number, skip: number): Promise<EntryCollection<PostFields>> {
