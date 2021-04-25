2cfcafffb4c5d8469d6bed28f838cae66a36ddfc
Honai Ueoka <git@honai.me>
2021-04-25 17:13:07 +0900

    add ads.txt

--- .eleventy.js
+++ .eleventy.js
@@ -3,9 +3,15 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight")
 const markdownIt = require('markdown-it')
 const markdownItKatex = require('@iktakahiro/markdown-it-katex')
 
+const fileCopies = ['images', 'favicon.ico', 'ads.txt']
+
 module.exports = (eleventyConfig) => {
-  eleventyConfig.setTemplateFormats(['html', 'md', 'pug', 'ejs', '11ty.js', 'ico'])
-  eleventyConfig.addPassthroughCopy('src/images')
+  eleventyConfig.setTemplateFormats(['html', 'md', 'pug', 'ejs', '11ty.js'])
+
+  for (const f of fileCopies) {
+    eleventyConfig.addPassthroughCopy(`src/${f}`)
+  }
+
   eleventyConfig.addFilter('sass', (code) => {
     return sass.renderSync({ data: code, outputStyle: 'compressed' }).css.toString()
   })
