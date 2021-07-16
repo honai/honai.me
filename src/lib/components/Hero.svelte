a5754dddb4ddf0d31a33e3187bd209062f481642
Honai Ueoka <git@honai.me>
2021-07-16 09:20:04 +0900

    Works移行

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,29 +1,44 @@
+<script>
+  export let title
+</script>
+
 <div class="nav-title">
-  <h1 class="title">Honai Ueoka's Portfolio</h1>
-  <nav class="nav">
-    <a href="/works">Works</a>
-    <a href="/blog/">Blog</a>
-  </nav>
+  {#if title}
+    <h2 class="subtitle">
+      <a href="/" class="_uncolor">Honai Ueoka's Portfolio</a>
+    </h2>
+    <h1 class="title">{title}</h1>
+  {:else}
+    <h1 class="title">Honai Ueoka's Portfolio</h1>
+    <nav class="nav">
+      <a href="/works">Works</a>
+      <a href="/blog/">Blog</a>
+    </nav>
+  {/if}
 </div>
 
-<style>
+<style lang="scss">
   .nav-title {
     padding: 1rem 0 3rem;
     font-family: var(--font-family-century);
     font-weight: var(--font-weight-century);
-  }
-  .nav-title > .title {
-    padding: 2rem 0;
-    text-align: center;
-  }
-  .nav-title > .nav {
-    font-size: 1.8rem;
-    display: flex;
-    width: 120px;
-    margin: 0 auto;
-    justify-content: space-between;
-  }
-  .nav-title > .nav > a {
-    color: unset;
+    > .subtitle {
+      font-size: 1.8rem;
+      text-align: center;
+    }
+    > .title {
+      padding: 2rem 0;
+      text-align: center;
+    }
+    > .nav {
+      font-size: 1.8rem;
+      display: flex;
+      width: 120px;
+      margin: 0 auto;
+      justify-content: space-between;
+      > a {
+        color: unset;
+      }
+    }
   }
 </style>
