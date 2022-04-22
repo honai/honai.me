1fc07f27cb0718dc51524ab10bd036a9b80dad87
honai <git@honai.me>
2022-04-22 17:57:57 +0900

    themeToggleをstitchesに移行

--- src/_includes/components/ThemeToggle.jsx
+++ src/_includes/components/ThemeToggle.jsx
@@ -1,10 +1,11 @@
+import { css } from "../style.mjs";
 import { SpanSvg } from "../svg";
 
 export const ThemeToggle = () => {
   return (
     <div id="theme-toggle" class="theme-toggle">
-      <SpanSvg class="icon" filename="theme" />{" "}
-      <select name="theme" id="theme-selector" class="select">
+      <SpanSvg class={svgIcon()} filename="theme" />{" "}
+      <select name="theme" id="theme-selector" class={select()}>
         <option value="system">System</option>
         <option value="light">Light</option>
         <option value="dark">Dark</option>
@@ -12,3 +13,17 @@ export const ThemeToggle = () => {
     </div>
   );
 };
+
+const svgIcon = css({
+  "> svg": {
+    verticalAlign: "middle",
+  },
+});
+
+const select = css({
+  color: "var(--color-text)",
+  backgroundColor: "var(--color-fg)",
+  border: "1px solid var(--color-border)",
+  borderRadius: "0.5rem",
+  padding: "0.5rem 0",
+});
