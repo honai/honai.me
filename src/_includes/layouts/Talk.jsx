cc0db088ed0f422c0dd250b5f72902cc7f193d97
honai <git@honai.me>
2022-05-04 03:25:48 +0900

    Talkページのスタイリング

--- src/_includes/layouts/Talk.jsx
+++ src/_includes/layouts/Talk.jsx
@@ -1,11 +1,34 @@
-import { css } from "../style.mjs";
+import { DateTag } from "../components/DateTag.jsx";
+import { css, uc } from "../style.mjs";
 import { PortfolioLayout } from "./PortfolioLayout";
 
 export default ({ page, content, title }) => {
   return (
     <PortfolioLayout pageUrl={page.url}>
-      <h1>{title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: content }} class={mdStyle()} />
+      <div
+        class={css({
+          width: "min(100% - 2rem, 72rem)",
+          margin: "0 auto",
+        })()}
+      >
+        <div
+          class={css({
+            textAlign: "center",
+            padding: "4rem 1rem",
+            display: "flex",
+            flexFlow: "column nowrap",
+            gap: "2rem",
+          })()}
+        >
+          <h1 class={css({ fontSize: "2.4rem", lineHeight: 1.25 })()}>
+            {title}
+          </h1>
+          <div class={css({ centuryGothic: true })()}>
+            <DateTag date={page.date} />
+          </div>
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: content }} class={mdStyle()} />
+      </div>
     </PortfolioLayout>
   );
 };
