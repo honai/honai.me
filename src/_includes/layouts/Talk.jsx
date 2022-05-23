4af3730db12298ce2aedde19a2a8ffe10a140512
honai <git@honai.me>
2022-05-23 19:11:04 +0900

    Improve slide page style

--- src/_includes/layouts/Talk.jsx
+++ src/_includes/layouts/Talk.jsx
@@ -1,4 +1,4 @@
-import { DateTag } from "../components/DateTag.jsx";
+import { TitleDate } from "../components/TitleDate.jsx";
 import { css } from "../style.mjs";
 import { PortfolioLayout } from "./PortfolioLayout";
 
@@ -16,22 +16,7 @@ export default ({ page, content, title, thumbnail }) => {
           margin: "0 auto",
         })()}
       >
-        <div
-          class={css({
-            textAlign: "center",
-            padding: "4rem 1rem",
-            display: "flex",
-            flexFlow: "column nowrap",
-            gap: "2rem",
-          })()}
-        >
-          <h1 class={css({ fontSize: "2.4rem", lineHeight: 1.25 })()}>
-            {title}
-          </h1>
-          <div class={css({ centuryGothic: true })()}>
-            <DateTag date={page.date} />
-          </div>
-        </div>
+        <TitleDate title={title} date={page.date} />
         <div dangerouslySetInnerHTML={{ __html: content }} class={mdStyle()} />
       </div>
     </PortfolioLayout>
--- src/slides/slide-page.jsx
+++ src/slides/slide-page.jsx
@@ -1,5 +1,6 @@
 import { DateTag } from "../_includes/components/DateTag";
 import { SlideCarousel } from "../_includes/components/SlideCarousel";
+import { TitleDate } from "../_includes/components/TitleDate";
 import { PortfolioLayout } from "../_includes/layouts/PortfolioLayout";
 import { css } from "../_includes/style.mjs";
 
@@ -12,10 +13,18 @@ export default ({ slideKey, slides, page }) => {
   const { title, date, pages } = slides[slideKey];
   const slidePages = pages.map((p) => ({ ...p, imageUrl: p.image }));
   return (
-    <PortfolioLayout pageUrl={page.url}>
-      <h2>{title}</h2>
-      <div>
-        <DateTag date={date} /> <span>{pages.length} ページ</span>
+    <PortfolioLayout
+      pageUrl={page.url}
+      subTitle={title}
+      thumbnailUrl={slidePages[0].imageUrl}
+    >
+      <div
+        class={css({
+          width: "min(100% - 2rem, 72rem)",
+          marginInline: "auto",
+        })()}
+      >
+        <TitleDate title={title} date={date} />
       </div>
       <div>
         <SlideCarousel pages={slidePages} />
