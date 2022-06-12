b1bbe162ce0cee5f4491a299184565c061ae5d30
honai <git@honai.me>
2022-06-13 07:29:25 +0900

    スライド上のリンクへはalways open with _blank

--- src/_includes/components/SlideCarousel/index.jsx
+++ src/_includes/components/SlideCarousel/index.jsx
@@ -1,5 +1,4 @@
 import { css, cx, darkTheme, uc } from "../../style.mjs";
-import { Link } from "../Link.jsx";
 import { Nav } from "./Nav.jsx";
 import * as AC from "../AvoidCache";
 
@@ -153,13 +152,14 @@ const SlideCarouselItem = ({
         />
         <>
           {links.map((/** @type {{url: string; position: any}} */ l) => (
-            <Link
+            <a
               href={l.url}
               class={linkOverlay()}
               style={{
                 ...l.position,
               }}
               title={l.url}
+              target="_blank"
             />
           ))}
         </>
