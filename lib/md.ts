b1683fbe42dca06031ccecd553e3e0ddce8e3e84
honai <git@honai.me>
2022-12-18 05:27:06 +0900

    add md plugin

--- lib/md.ts
+++ lib/md.ts
@@ -1,4 +1,9 @@
 import mdIt from "markdown-it";
+import anchor from "markdown-it-anchor";
+import linkAttrs from "markdown-it-link-attributes";
+// 型定義ない
+import footnote from "markdown-it-footnote";
+import katex from "@iktakahiro/markdown-it-katex";
 // ESModule用のmodule型定義に対応していない
 import hljs from "highlight.js";
 
@@ -6,12 +11,12 @@ const simpleInstance = mdIt({ html: true });
 
 export const render = (md: string) => simpleInstance.render(md);
 
-const postInstance = mdIt({
+// TODO: footnote, anchor, link attr
+const postInstanceInit = mdIt({
   html: true,
   linkify: true,
   highlight: (str, lang) => {
     if (!lang) {
-      // todo escape
       return `<pre><code class="hljs">${postInstance.utils.escapeHtml(
         str
       )}</code></pre>`;
@@ -23,5 +28,27 @@ const postInstance = mdIt({
     throw `unsupported codefence language: ${lang}`;
   },
 }) as mdIt; // 引数内で自身にアクセスしているためanyになる
+
+const postInstance = postInstanceInit
+  .use(footnote)
+  .use(anchor)
+  .use(katex)
+  .use(linkAttrs, {
+    // 自サイトとブログ以外のリンクを target blank
+    matcher(href: string) {
+      const hasScheme = /^(https?:)?\/\//;
+      if (!hasScheme.test(href)) {
+        // 相対パス or 絶対パス or fragment
+        return false;
+      }
+      const sameDomainUrl =
+        /^(https?:)?\/\/((www\.)|(blog\.)|())honai\.me($|[^.])/;
+      return !sameDomainUrl.test(href);
+    },
+    attrs: {
+      target: "_blank",
+      rel: "noopener",
+    },
+  });
 
 export const renderPost = (content: string) => postInstance.render(content);
